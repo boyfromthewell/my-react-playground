fc9c745d571fa78c45d76e188bd01d5c8623376a
boyfromthewell <tnsdyd10@naver.com>
2023-04-18 17:19:18 +0900

    feat: useDefferredValue 설명 추가

--- src/pages/useDefferredValue/afterApplyingHook.tsx
+++ src/pages/useDefferredValue/afterApplyingHook.tsx
@@ -13,13 +13,6 @@ const AfterApplyingHook = () => {
 	console.log("value", value);
 	console.log("deferredValue", deferredValue);
 
-	/* 
-	input에 있는 값은 빠르게 변하지만 deferredValue의 값은 바로 변경되지 않음
-	deferredValue에 의존하는 boxes 연산은 value보다 긴급하지 않기 때문에 value 값만 먼저 업데이트 하고 deferredValue 값은 지연
-	즉, deferredValue에 의해 boxes 연산이 진행되는 와중에 value가 업데이트 되면 boxes 렌더링을 interrupt하고 value만 먼저 렌더링
-	이제 input의 value는 즉각적으로 변하지만, deferredValue와 boxes는 그보다 낮은 빈도수로 업데이트 되고 렌더링 차단은 일어나지 않게 됨
-	*/
-
 	const boxes = useMemo(() => {
 		return (
 			<BoxContainer>
@@ -36,6 +29,21 @@ const AfterApplyingHook = () => {
 
 	return (
 		<PageContainer>
+			<Description>
+				<p>input에 있는 값은 빠르게 변하지만 deferredValue의 값은 바로 변경되지 않음</p>
+				<p>
+					deferredValue에 의존하는 boxes 연산은 value보다 긴급하지 않기 때문에 value 값만 먼저 업데이트 하고
+					deferredValue 값은 지연
+				</p>
+				<p>
+					즉, deferredValue에 의해 boxes 연산이 진행되는 와중에 value가 업데이트 되면 boxes 렌더링을 interrupt하고
+					value만 먼저 렌더링
+				</p>
+				<p>
+					이제 input의 value는 즉각적으로 변하지만, deferredValue와 boxes는 그보다 낮은 빈도수로 업데이트 되고 렌더링
+					차단은 일어나지 않게 됨
+				</p>
+			</Description>
 			<Input onChange={onChange} /> {boxes}
 		</PageContainer>
 	);
@@ -52,4 +60,10 @@ const BoxContainer = styled.div`
 
 const Input = styled.input`
 	height: 100px;
+`;
+
+const Description = styled.section`
+	display: flex;
+	flex-direction: column;
+	line-height: 120%;
 `;
--- src/pages/useDefferredValue/beforeApplyingHook.tsx
+++ src/pages/useDefferredValue/beforeApplyingHook.tsx
@@ -8,16 +8,6 @@ const BeforeApplyingHook = () => {
 		setValue(e.target.value);
 	};
 
-	/* 
-    인풋을 입력할 때마다 박스의 배경색을 모두 계산
-    타이핑을 빠르게 친다면 렌더링 성능 저하를 유발하는 코드 (렌더링 차단)
-    이런 상황에서는 보통 디바운스/쓰로트링 기법을 사용
-    (하지만 debounce와 throttle이 완벽한 UX를 보장해주지는 않음)
-    이러한 상황을 해결하기 위해 나온 것이 React 18의 Concurrent Mode이며, useDeferredValue hook을 이용하여 deferredValue의 값을 지연시킬 수 있다.
-
-	출처 : https://vroomfan.tistory.com/45
-    */
-
 	const boxes = useMemo(() => {
 		return (
 			<BoxContainer>
@@ -34,7 +24,23 @@ const BeforeApplyingHook = () => {
 
 	return (
 		<PageContainer>
-			<Input onChange={onChange} /> {boxes}
+			<Description>
+				{" "}
+				<p>
+					인풋을 입력할 때마다 박스의 배경색을 모두 계산 타이핑을 빠르게 친다면 렌더링 성능 저하를 유발하는 코드 (렌더링
+					차단)
+				</p>
+				<p>
+					이런 상황에서는 보통 디바운스/쓰로트링 기법을 사용 (하지만 debounce와 throttle이 완벽한 UX를 보장해주지는
+					않음)
+				</p>{" "}
+				<p>
+					이러한 상황을 해결하기 위해 나온 것이 React 18의 Concurrent Mode이며, useDeferredValue hook을 이용하여
+					deferredValue의 값을 지연시킬 수 있다.
+				</p>
+				출처 : https://vroomfan.tistory.com/45
+			</Description>
+			<Input onChange={onChange} /> {boxes}{" "}
 		</PageContainer>
 	);
 };
@@ -50,4 +56,10 @@ const BoxContainer = styled.div`
 
 const Input = styled.input`
 	height: 100px;
+`;
+
+const Description = styled.section`
+	display: flex;
+	flex-direction: column;
+	line-height: 120%;
 `;
